747fef6cb980efed523f7da7d2519d93109383d4
Ratheesh K <ratheeshk2505@gmail.com>
2021-11-20 17:49:37 +0530

    server file change

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/weather'));
+app.use(express.static('./dist/weather-api'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/weather/'}),
+    res.sendFile('index.html', {root: 'dist/weather-api/'}),
 );
 
 // Start the app by listening on the default Heroku port
